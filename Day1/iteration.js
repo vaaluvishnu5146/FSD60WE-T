c54e26bf1299ee3eef04b2870972281b333f2992
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-06-02 12:52:34 +0530

    Added forEach to iteration

--- Day1/iteration.js
+++ Day1/iteration.js
@@ -1,4 +1,10 @@
 /**
+ * Array methods are plenty in javascript
+ * Basic for loop will helps us to loop through any range of numbers and elements of array with any step.
+ * There are other for loop which will help us to loop through all the elements without jump
+ */
+/**
+ * Types of loops:
  * 1. For...in
  * 2. For...of
  * 3. forEach
@@ -21,5 +27,14 @@ var data = {
   age: 28,
 };
 for (let y in data) {
-  console.log(data[y]);
+  console.log(y, "=", data[y]);
 }
+
+/**
+ * forEach - Available in prototypal chain
+ */
+var scores = [1, 2, 1, 2, 3, 4];
+
+scores.forEach(function (value, index) {
+  console.log("Element ===>", value, index);
+}); // CALLBACK FUNCTION
